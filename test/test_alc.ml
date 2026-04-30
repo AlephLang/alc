8885a8bf61c3f1ca1f57ef6e4c418c7c7492087f
Nyvyme <mixagame228@gmail.com>
2026-05-01 03:09:20 +0500

    Prettyprint tests.

---
+++ test/test_alc.ml
@@ -0,0 +1,14 @@
+open Alcotest
+
+let int_to_bin_str x expected () =
+  let got = (Printf.sprintf "%a" Alc.Util.pp_binint x) in
+  check string "same strings" expected got
+
+let suite =
+  [ "int to binary string (123)", `Quick, int_to_bin_str 123 "1111011"
+  ; "int to binary string (-123)", `Quick, int_to_bin_str (-123)
+     "111111111111111111111111111111111111111111111111111111110000101"
+  ]
+
+let () =
+  Alcotest.run "Util tests" [ "Int to binary string", suite ]

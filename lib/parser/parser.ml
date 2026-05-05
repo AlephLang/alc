578a566030ad3d8bd9dfa349cf7ccbfccb2de6f7
Nyvyme <mixagame228@gmail.com>
2026-05-06 03:11:02 +0500

    Parser: Move '_toplevels' function away from function 'parse'.

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -1,17 +1,17 @@
 include Parser_core
 open Parser_top
 
-let parse parser =
-  let rec _toplevels p =
-    if p.pos >= List.length p.tokens then p, []
-    else
-      let p, toplevel = parse_top p in
-      let p, remaining = _toplevels p in
-      match toplevel with
-      | None -> p, remaining
-      | Some x -> p, x :: remaining in
+let rec parse_toplevels p =
+  if p.pos >= List.length p.tokens then p, []
+  else
+    let p, toplevel = parse_top p in
+    let p, remaining = parse_toplevels p in
+    match toplevel with
+    | None -> p, remaining
+    | Some x -> p, x :: remaining
 
-  let parser, toplevels = _toplevels parser in
+let parse parser =
+  let parser, toplevels = parse_toplevels parser in
   let root : Ast.t = {
     kind = Ast.Root { toplevel_statements = toplevels };
     pos = 0;

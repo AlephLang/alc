7a550604177988592d84e04c3a8166b85e95b711
Nyvyme <mixagame228@gmail.com>
2026-06-20 16:24:28 +0500

    Parser: Defer statement.

--- libalc/parser/stmt_defer.c
+++ libalc/parser/stmt_defer.c
@@ -1,8 +1,21 @@
+#include "alc/ast.h"
 #include "alc/defs.h"
+#include "allocs/alloc_arena.h"
+#include "global.h"
 #include "parser/parser_private.h"
 
 alc_ast_t *parse_stmt_defer(alc_parser_t *p)
 {
-  ALC_UNUSED_DEBUG(p);
-  ALC_TODO("Parse defer statement");
+  ALC_ASSUME(p != nullptr);
+
+  usize pos = p->pos++;
+
+  alc_ast_t *body = parse_stmt(p);
+  _VERIFY_AST(body);
+
+  alc_ast_t *defer_stmt_ast = alloc_arena_allocate(&ctx()->arena, sizeof(alc_ast_t));
+  defer_stmt_ast->data.STMT_DEFER.body = body;
+  defer_stmt_ast->pos = pos;
+  defer_stmt_ast->kind = ALC_AST_KIND_STMT_DEFER;
+  return defer_stmt_ast;
 }
